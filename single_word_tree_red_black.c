791ef3b8f065fcedd3bd9ae9cef02eec65e5f7e6
Furrrlo <ferloo@protonmail.com>
2022-06-08 13:43:38 +0200

    Implement word_tree storing children in a rbtree

---
+++ single_word_tree_red_black.c
@@ -0,0 +1,733 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+#define LAMBDA(ret_type, _body) ({ ret_type _ _body _; })
+
+#define ALPHABETH_SIZE (int)((sizeof(ALPHABETH)-1)/sizeof(char))
+
+// ASCII ordered
+char ALPHABETH[] = "-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz";
+
+char pos_to_char(int pos)
+{
+  return ALPHABETH[pos];
+}
+
+int char_to_pos(char c)
+{
+  // This is to make it O(1)
+  int offset = 0;
+  if(c == '-')
+    return offset;
+  ++offset;
+  
+  if(c >= '0' && c <= '9')
+    return offset + (c - '0');
+  offset += ('9' - '0') + 1;
+  
+  if(c >= 'A' && c <= 'Z')
+    return offset + (c - 'A');
+  offset += ('Z' - 'A') + 1;
+
+  if(c == '_')
+    return offset;
+  ++offset;
+
+  if(c >= 'a' && c <= 'z')
+    return offset + (c - 'a');
+  offset += ('z' - 'a') + 1;
+  
+#ifdef DEBUG
+  // Fallback to looping just to make sure
+  for(int i = 0; i < ALPHABETH_SIZE; ++i)
+   if(ALPHABETH[i] == c)
+    return i;
+#endif 
+
+  return -1;
+}
+
+struct rb_tree;
+typedef struct word_tree_node 
+{
+  struct rb_tree *children;
+} word_tree_t;
+
+#define BLACK 0
+#define RED 1
+
+typedef struct rb_tree
+{
+  char key;
+  struct word_tree_node value;
+
+  unsigned char color;
+  struct rb_tree *parent; 
+  struct rb_tree *right;
+  struct rb_tree *left;
+} *rb_tree_t;
+
+struct rb_tree *new_rb_tree()
+{
+  return NULL;
+}
+
+void rb_tree_free(rb_tree_t tree)
+{
+  if(tree == NULL)
+    return;
+
+  rb_tree_free(tree->value.children);
+  rb_tree_free(tree->right);
+  rb_tree_free(tree->left);
+  free(tree);
+}
+
+int rb_tree_is_empty(rb_tree_t tree)
+{
+  return tree == NULL;
+}
+
+void rb_tree_for_each_ordered(rb_tree_t tree, void (*func)(char, word_tree_t*))
+{
+  if(rb_tree_is_empty(tree))
+      return;
+
+  rb_tree_for_each_ordered(tree->left, func);
+  func(tree->key, &tree->value);
+  rb_tree_for_each_ordered(tree->right, func);
+}
+
+struct word_tree_node *rb_tree_get(rb_tree_t tree, char key)
+{
+  if(rb_tree_is_empty(tree))
+    return NULL;
+
+  if(tree->key == key)
+    return &tree->value;
+
+  if(key < tree->key)
+    return rb_tree_get(tree->left, key);
+  return rb_tree_get(tree->right, key);
+}
+
+struct rb_tree *rb_tree_bst(rb_tree_t *tree, struct rb_tree *node, int allow_duplicates)
+{
+  if(rb_tree_is_empty(*tree))
+  {
+    *tree = node;
+    return NULL;
+  }
+
+  if(!allow_duplicates && node->key == (*tree)->key)
+    return node;
+
+  if(node->key < (*tree)->key)
+  {
+    struct rb_tree *res = rb_tree_bst(&(*tree)->left, node, allow_duplicates);
+    if(res == NULL)
+      (*tree)->left->parent = *tree;
+    return res;
+  }
+  
+  struct rb_tree *res = rb_tree_bst(&(*tree)->right, node, allow_duplicates);
+  if(res == NULL)
+    (*tree)->right->parent = *tree;
+  return res;
+}
+
+void rb_tree_rrotate(rb_tree_t *tree, struct rb_tree *node)
+{
+    struct rb_tree *left = node->left;
+    node->left = left->right;
+    if(node->left)
+      node->left->parent = node;
+    left->parent = node->parent;
+    if (!node->parent)
+      *tree = left;
+    else if (node == node->parent->left)
+      node->parent->left = left;
+    else
+      node->parent->right = left;
+    left->right = node;
+    node->parent = left;
+}
+
+void rb_tree_lrotate(rb_tree_t *tree, struct rb_tree *node)
+{
+    struct rb_tree *right = node->right;
+    node->right = right->left;
+    if(node->right)
+      node->right->parent = node;
+    right->parent = node->parent;
+    if(!node->parent)
+      *tree = right;
+    else if (node == node->parent->left)
+      node->parent->left = right;
+    else
+      node->parent->right = right;
+    right->left = node;
+    node->parent = right;
+}
+
+void rb_tree_fixup(rb_tree_t *tree, struct rb_tree *node)
+{
+  struct rb_tree *root = *tree;
+  struct rb_tree *parent = NULL;
+  struct rb_tree *grand_parent = NULL;
+
+  while(node != root && node->color != BLACK && node->parent->color == RED)
+  {
+    parent = node->parent;
+    grand_parent = node->parent->parent;
+
+    if(parent == grand_parent->left)
+    {
+      struct rb_tree *uncle = grand_parent->right;
+
+      if(uncle != NULL && uncle->color == RED)
+      {
+        grand_parent->color = RED;
+        parent->color = BLACK;
+        uncle->color = BLACK;
+        node = grand_parent;
+        continue;
+      }
+
+      // uncle == NULL || uncle->color == BLACK
+      if(node == parent->right)
+      {
+        rb_tree_lrotate(tree, parent);
+        node = parent;
+        parent = node->parent;
+      }
+
+      rb_tree_rrotate(tree, grand_parent);
+      int tmp = parent->color;
+      parent->color = grand_parent->color;
+      grand_parent->color = tmp;
+      node = parent;
+      continue;
+    }
+
+    // parent == grand_parent->right
+    struct rb_tree *uncle = grand_parent->left;
+
+    if(uncle != NULL && uncle->color == RED)
+    {
+      grand_parent->color = RED;
+      parent->color = BLACK;
+      uncle->color = BLACK;
+      node = grand_parent;
+      continue;
+    }
+     
+    // uncle == NULL || uncle->color == BLACK 
+    if(node == parent->left)
+    {
+      rb_tree_rrotate(tree, parent);
+      node = parent;
+      parent = node->parent;
+    }
+
+    rb_tree_lrotate(tree, grand_parent);
+    int tmp = parent->color;
+    parent->color = grand_parent->color;
+    grand_parent->color = tmp;
+    node = parent;
+  }
+
+  root->color = BLACK;
+}
+
+struct word_tree_node *rb_tree_do_put(rb_tree_t *tree, char c, int allow_duplicates)
+{
+  struct rb_tree *new_node = (struct rb_tree*) malloc(sizeof(struct rb_tree));
+#ifdef DEBUG
+  if(new_node == NULL)
+  {
+    printf("Failed to allocate rb_tree\n");
+    exit(-40);
+    return NULL;
+  }
+#endif
+  
+  new_node->key = c;
+  new_node->value.children = new_rb_tree();
+  new_node->color = RED;
+  new_node->right = NULL;
+  new_node->left = NULL;
+  new_node->parent = NULL;
+
+  struct rb_tree *already_present = rb_tree_bst(tree, new_node, allow_duplicates);
+  if(already_present != NULL)
+  {
+    rb_tree_free(new_node);
+    return &already_present->value;
+  }
+
+  rb_tree_fixup(tree, new_node);
+  return &new_node->value;
+}
+
+struct word_tree_node *rb_tree_put(rb_tree_t *tree, char c)
+{
+  return rb_tree_do_put(tree, c, 1);
+}
+
+struct word_tree_node *rb_tree_put_if_absent(rb_tree_t *tree, char c)
+{
+  // TODO: fix rb_tree_do_put 
+  word_tree_t *child = rb_tree_get(*tree, c);
+  if(child == NULL)
+    child = rb_tree_put(tree, c);
+  return child;
+}
+
+word_tree_t *new_word_tree()
+{
+  word_tree_t *ref = (word_tree_t*) malloc(sizeof(word_tree_t));
+#ifdef DEBUG
+  if(ref == NULL)
+  {
+    printf("Couldn't allocate word_tree_t\n");
+    exit(-3);
+    return NULL;
+  }
+#endif
+
+  ref->children = new_rb_tree();
+  return ref;
+}
+
+void word_tree_free(word_tree_t *tree)
+{
+  if(tree == NULL)
+    return;
+
+  rb_tree_free(tree->children);
+  free(tree);
+}
+
+/* O(len) */
+int word_tree_contains(const word_tree_t *tree, char *str)
+{
+  const word_tree_t *subtree = tree;
+  for(int i = 0; str[i]; ++i)
+  {
+    const word_tree_t *child = rb_tree_get(subtree->children, str[i]);
+    if(child == NULL)
+      return 0;
+
+    subtree = child;
+  }
+
+  return 1;
+}
+
+#define VISIT_SUBTREE 1
+#define SKIP_SUBTREE 2
+
+int word_tree_push_helper(word_tree_t *tree, char *str, int i)
+{
+  if(!str[i])
+    return 1;
+
+  int pos = char_to_pos(str[i]);
+  if(pos == -1)
+    return 0;
+
+  word_tree_t *child = rb_tree_put_if_absent(&tree->children, str[i]);
+  return word_tree_push_helper(child, str, i + 1);
+}
+
+int word_tree_push(word_tree_t *tree, char *str)
+{
+  return word_tree_push_helper(tree, str, 0);
+}
+
+void word_tree_for_each_ordered_helper(word_tree_t *tree, 
+                                       int pos, int len, 
+                                       char *str, int freq[ALPHABETH_SIZE], 
+                                       int (*filter)(int, char, int),
+                                       void (*func)(char*, int*))
+{
+  rb_tree_for_each_ordered(tree->children, LAMBDA(void, (char c, word_tree_t *child) {
+    int i = char_to_pos(c);
+    int filter_res = filter(pos, c, freq[i] + 1);
+    if(filter_res == SKIP_SUBTREE)
+      return;
+
+    str[pos] = c;
+    if(pos + 1 < len)
+    {
+      freq[i]++;
+      word_tree_for_each_ordered_helper(child, pos + 1, len, str, freq, filter, func);
+      freq[i]--;
+      return;
+    }
+
+    freq[i]++;
+    func(str, freq); 
+    freq[i]--;
+  }));
+}
+
+void word_tree_for_each_ordered(word_tree_t *tree, 
+                                int len,
+                                int (*filter)(int, char, int), 
+                                void (*func)(char*, int*))
+{
+  char word[len + 1];
+  word[len] = '\0';
+  int freq[ALPHABETH_SIZE] = {0};
+  word_tree_for_each_ordered_helper(tree, 0, len, word, freq, filter, func);
+}
+
+// Time - Theta(n)
+void populate_freq(char *word, int len, int new_freq[ALPHABETH_SIZE])
+{
+  // Populate frequencies of the word we were just given
+  memset(new_freq, 0, ALPHABETH_SIZE * sizeof(int));
+  for(int i = 0; i < len; ++i)
+    new_freq[char_to_pos(word[i])]++;
+}
+
+typedef struct {
+  char *word;
+  int len;
+  int freq[ALPHABETH_SIZE];
+  char *found_chars;
+  char **found_not_chars;
+  int found_freq_min[ALPHABETH_SIZE];
+  int found_freq_max[ALPHABETH_SIZE];
+} reference_t;
+
+void init_ref(reference_t *ref, char *word, int len)
+{
+  ref->word = word;
+  ref->len = len;
+  populate_freq(ref->word, ref->len, ref->freq);
+  ref->found_chars = (char*) malloc(ref->len * sizeof(char));
+  memset(ref->found_chars, 0, ref->len * sizeof(char));
+
+  ref->found_not_chars = (char**) malloc(ref->len * sizeof(char*));
+  for(int i = 0; i < ref->len; i++)
+  {
+    ref->found_not_chars[i] = (char*) malloc((ALPHABETH_SIZE + 1) * sizeof(char));
+    ref->found_not_chars[i][0] = '\0';
+  }
+
+  memset(ref->found_freq_min, 0, sizeof(ref->found_freq_min));
+  memset(ref->found_freq_max, -1, sizeof(ref->found_freq_max));
+}
+
+void ref_dispose(reference_t *ref)
+{
+  free(ref->found_chars);
+  for(int i = 0; i < ref->len; ++i)
+    free(ref->found_not_chars[i]);
+  free(ref->found_not_chars);
+}
+
+void print_found_ref(reference_t *ref)
+{
+  printf("letters: \"");
+  for(int i = 0; i < ref->len; ++i)
+    printf("%c", ref->found_chars[i] == 0 ? ' ' : ref->found_chars[i]);
+  printf("\"\n");
+
+  printf("not: {");
+  for(int i = 0; i < ref->len; ++i)
+    if(ref->found_not_chars[i] != NULL)
+      printf("%d: \"%s\", ", i, ref->found_not_chars[i]);
+  printf("}\n");
+
+  printf("min freq: {");
+  for(int i = 0; i < ALPHABETH_SIZE; ++i)
+    if(ref->found_freq_min[i] > 0)
+     printf("'%c': %d, ", pos_to_char(i), ref->found_freq_min[i]);
+  printf("}\n");
+  
+  printf("max freq: {");
+  for(int i = 0; i < ALPHABETH_SIZE; ++i)
+    if(ref->found_freq_max[i] >= 0)
+     printf("'%c': %d, ", pos_to_char(i), ref->found_freq_max[i]);
+  printf("}\n");
+}
+
+/* Time - Theta(n) */
+void compare_words(reference_t *ref, char *new, char *out)
+{
+  // Spatial - Stack allocated, constant
+  // Time - Theta(n)
+  int new_freq[ALPHABETH_SIZE] = {0};
+  memcpy(new_freq, ref->freq, sizeof(new_freq));
+
+  // First pass, find letters which are the same
+  // Theta(n)
+  for(int i = 0; i < ref->len; ++i)
+  {
+    if(ref->word[i] != new[i])
+      continue;
+
+    ref->found_chars[i] = ref->word[i];
+    out[i] = '+';
+
+    int pos = char_to_pos(new[i]);
+    --new_freq[pos];
+    ref->found_freq_min[pos] = MAX(ref->found_freq_min[pos], ref->freq[pos] - new_freq[pos]);
+  }
+
+  // Second pass, letters which are wrong or at the wrong place
+  // Theta(n)
+  for(int i = 0; i < ref->len; ++i)
+  {
+    if(ref->word[i] == new[i])
+      continue;
+
+    int pos = char_to_pos(new[i]);
+    int found_not_char = 0;
+    if(new_freq[pos] <= 0)
+    {
+      out[i] = '/';
+      found_not_char = ref->freq[pos] > 0;
+      ref->found_freq_max[pos] = ref->freq[pos];
+    }
+    else
+    {
+      out[i] = '|';
+      found_not_char = 1;
+      --new_freq[pos];
+      ref->found_freq_min[pos] = MAX(ref->found_freq_min[pos], ref->freq[pos] - new_freq[pos]);
+    }
+
+    if(found_not_char)
+    {
+      char *str = ref->found_not_chars[i];
+      char to_append = new[i];
+
+      int len;
+      int append = 1;
+      for(len = 0; str[len]; ++len)
+      {
+        if(str[len] == to_append)
+        {
+          append = 0;
+          break;
+        }
+      }
+      
+      if(append)
+      {
+        str[len] = to_append;
+        str[len + 1] = '\0';
+      }
+    }
+  }
+}
+
+void filter_dictionary(reference_t *ref, word_tree_t *tree, void (*func)(char*))
+{
+  int (*known_chars_filter)(int, char, int) = LAMBDA(int, (int pos, char c, int char_freq) {
+    
+    if(ref->found_chars[pos] != 0 && ref->found_chars[pos] != c)
+      return SKIP_SUBTREE;
+
+    if(ref->found_not_chars[pos])
+    {
+      char *found_not_chars = ref->found_not_chars[pos];
+      // Max ALPHABETH_SIZE, so should be fairly small
+      for(int k = 0; found_not_chars[k]; ++k)
+        if(c == found_not_chars[k])
+          return SKIP_SUBTREE;
+    }
+
+    int alphabeth_pos = char_to_pos(c);
+    if(ref->found_freq_max[alphabeth_pos] >= 0 && char_freq > ref->found_freq_max[alphabeth_pos])
+      return SKIP_SUBTREE;
+
+    return VISIT_SUBTREE;
+  });
+  int (*words_filter)(char*, int*) = LAMBDA(int, (char *str, int *freq) { 
+      for(int i = 0; i < ALPHABETH_SIZE; ++i)
+      {
+        if(freq[i] < ref->found_freq_min[i])
+          return 0;
+      }
+
+      return 1; 
+  }); 
+
+  // Theta(n)
+  word_tree_for_each_ordered(tree, ref->len, known_chars_filter, LAMBDA(void, (char *str, int *freq) {
+    if(words_filter(str, freq))
+      func(str);
+  }));
+}
+
+void populate_dictionary(word_tree_t *tree, int len, char *stop_command)
+{
+  char line[MAX(32, len) + 1];
+  for(;;)
+  {
+    if(scanf("%s", line) < 0)
+    {
+      printf("Failed to read dict line\n");
+      exit(-52);
+      return;
+    }
+
+    if(strcmp(stop_command, line) == 0)
+		  break;
+
+#ifdef DEBUG
+    if(len != strlen(line))
+    {
+      printf("Invalid word length %ld, expected %d for %s\n", strlen(line), len, line);
+      exit(-6);
+      return;
+    }
+#endif
+
+    word_tree_push(tree, line);
+  }
+}
+
+void check_everything_allrite()
+{
+#ifdef DEBUG
+  int error = 0;
+  for(int i = 0; i < ALPHABETH_SIZE; ++i)
+  {
+    if(ALPHABETH[i] != pos_to_char(char_to_pos(ALPHABETH[i])))
+    {
+      printf("error in alphabeth conversion: %c %d %c\n", 
+          ALPHABETH[i],
+          char_to_pos(ALPHABETH[i]), 
+          pos_to_char(char_to_pos(ALPHABETH[i])));
+      error = 1;
+    }
+  }
+
+  // Check that the alphabeth is ascii ordered
+  for(int i = 0; i < ALPHABETH_SIZE - 1; ++i)
+  {
+    if(pos_to_char(i) >= pos_to_char(i + 1))
+    {
+      printf("Invalid ASCII order in alphabeth: %c >= %c\n", pos_to_char(i), pos_to_char(i + 1));
+      error = 1;
+    }
+  }
+  
+  if(!error)
+    return;
+
+  printf("precondition error, fix it\n");
+  exit(-2);
+#endif
+}
+
+int main()
+{
+  check_everything_allrite();
+
+  int len;
+  if(scanf("%d", &len) < 0)
+  {
+    printf("Failed to read len\n");
+    return -50;
+  }
+
+  if(len <= 0)
+    return 0;
+  
+  char *out = (char*) malloc((len + 1) * sizeof(char));
+  out[len] = '\0';
+
+  word_tree_t *tree = new_word_tree();
+  populate_dictionary(tree, len, "+nuova_partita");
+  
+  for(int quit = 0; !quit; )
+  {  
+    char ref_str[len + 1];
+    int max_guesses;
+    if(scanf("%s %d\n", ref_str, &max_guesses) < 0)
+    {
+      printf("Failed to read guesses\n");
+      return -51;
+    }
+
+    if(max_guesses <= 0)
+      return 0;
+
+    reference_t ref;
+    init_ref(&ref, ref_str, len);
+
+    char line[MAX(len, 32) + 1];
+   
+    int tries = 0;
+    int game_over = 0;
+    for(;;)
+    {
+      if(scanf("%s", line) < 0)
+      {
+        goto quit_program;
+      }
+
+      // printf("Read %s\n", line);
+      if(game_over || line[0] == '+')
+      {
+        if(strcmp(line, "+nuova_partita") == 0)
+        {
+          goto new_game;
+        }
+        else if(strcmp(line, "+inserisci_inizio") == 0)
+        {
+          populate_dictionary(tree, len, "+inserisci_fine");
+        }
+        else if(strcmp(line, "+stampa_filtrate") == 0)
+        {
+          // print_found_ref(&ref);
+          filter_dictionary(&ref, tree, LAMBDA(void, (char *str) { printf("%s\n", str); }));
+        }
+      }
+      else if(strcmp(ref.word, line) == 0)
+      {
+        printf("ok\n");
+        game_over = 1;
+      }
+      else if(!word_tree_contains(tree, line)) 
+      {
+        printf("not_exists\n");
+      }
+      else
+      {
+        compare_words(&ref, line, out);
+        printf("%s\n", out);
+
+        size_t size = 0;
+        filter_dictionary(&ref, tree, LAMBDA(void, (char *str) { size++; }));
+        printf("%ld\n", size);
+
+        if(++tries >= max_guesses)
+        {
+          printf("ko\n");
+          game_over = 1;
+        }
+      }
+    }
+
+quit_program:
+    quit = 1;
+new_game:
+    ref_dispose(&ref);
+  }
+
+  free(out);
+  word_tree_free(tree);
+  return 0;
+}
